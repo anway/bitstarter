3f977a2df7b7caf4892a5ad26dc19931dd7c9b4e
Annie Wei <anniewei@sbcglobal.net>
2013-08-05 21:08:18 -0700

    Added alt and some row-fluids

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var FILE_READIN = "infile.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,14 +64,38 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var processfile = function(file, checks) {
+    var checkJson = checkHtmlFile(file, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+    fs.writeFile('submit.json', outJson);
+};
+
+var assertURLExists = function(url, resultfile, checks) {
+    var response = function(result, response) {
+	if (result instanceof Error) {
+	    console.log('Error: ' + util.format(response.message));
+	} else {
+	    resultfile = resultfile || FILE_READIN;
+	    fs.writeFileSync(resultfile, result);
+	    var instr = assertFileExists(resultfile);
+	    processfile(instr, checks);
+	}
+    };
+    rest.get(url).on('complete', response);
+};
+
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-u, --url <url>', 'URL of file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+	assertURLExists(program.url, program.file, program.checks);
+    } else {
+	processfile(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
